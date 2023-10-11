524165c11484acf2e1b58e74598a578681b3d613
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2023-10-12 01:49:54 +0530

    Add | Create Admin menu

--- instructions.h
+++ instructions.h
@@ -15,4 +15,19 @@
 "======================================================\n" \
 "Please enter your choice : "
 
+#define ADMIN_MENU \
+"================= Select the Operation ===============\n" \
+"=                                                    =\n" \
+"=  1.  Add a Student                                 =\n" \
+"=  2.  View Student Details                          =\n" \
+"=  3.  Add a Faculty                                 =\n" \
+"=  2.  View Faculty Details                          =\n" \
+"=  1.  Activate a Student                            =\n" \
+"=  1.  De-Activate a Student                         =\n" \
+"=  2.  Modify Student Details                        =\n" \
+"=  2.  Modify Faculty Details                        =\n" \
+"=                                                    =\n" \
+"======================================================\n" \
+"Please enter your choice : "
+
 #endif
