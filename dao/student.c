cd554cb52ee1a1a04b4b9b2e5d05f24a1515e965
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2023-10-12 20:32:02 +0530

    Add(module/dao) | create student functionality

---
+++ dao/student.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "student-dao.h"
+#include "../model/student.h"
+
+void generateStudentId(char* new_id);
+void generateStudentPassword(char* password);
+void incrementRollNumber(const char* input_rollno, char* new_rollno, int size);
+
+void insertStudent(char name[], int age, char email[]) {
+    char new_id[6], password[50];
+    generateStudentId(new_id);
+    generateStudentPassword(password);
+    printf("%s \n", new_id);
+    printf("%s \n", password);
+    struct Student student;
+    bzero(&student, sizeof(student));
+    strcpy(student.std_id, new_id);
+    strcpy(student.name, name);
+    student.age = age;
+    strcpy(student.email, email);
+    strcpy(student.password, password);
+    student.no_of_courses_enrolled = 0;
+    printf("id: %s \n", student.std_id);
+    printf("name: %s \n", student.name);
+    printf("age: %d \n", student.age);
+    printf("email: %s \n", student.email);
+    int fd = open("/Users/nishthapaul/iiitb/academia-portal/data/student.txt", O_WRONLY | O_APPEND);
+    if (fd == -1) {
+		perror("Error in opening the file student.txt. \n");
+	}
+    if(write(fd, &student, sizeof(struct Student)) == -1) {
+        perror("Student details could not be written to the file");
+    }
+    close(fd);
+}
+
+void generateStudentId(char* new_id) {
+    int fd = open("/Users/nishthapaul/iiitb/academia-portal/data/student.txt", O_RDONLY);
+    if (fd == -1) {
+		perror("Error in opening the file student.txt. \n");
+	}
+    struct Student temp;
+    int offset = lseek(fd, -1 * sizeof(struct Student), SEEK_END);
+    printf("offset: %d \n", offset);
+    // if file is empty, then it is the first student
+    if (offset == -1) {
+        sprintf(new_id, "MT001");
+        printf("Inside gen: %s \n", new_id);
+        return;
+    }
+    read(fd, &temp, sizeof(struct Student));
+    close(fd);
+    char last_id[6];
+    strcpy(last_id, temp.std_id);
+    incrementRollNumber(last_id, new_id, sizeof(new_id));
+}
+
+void generateStudentPassword(char* password) {
+    sprintf(password, "password");
+}
+
+void incrementRollNumber(const char* input_rollno, char* new_rollno, int size) {
+    char prefix[3];
+    int numeric_part;
+    sscanf(input_rollno, "%2s%d", prefix, &numeric_part);
+    numeric_part++;
+    snprintf(new_rollno, size, "%s%03d", prefix, numeric_part);
+    printf("New roll: %s\n", new_rollno);
+}
--- module/admin.c
+++ module/admin.c
@@ -1,8 +1,17 @@
-#include "admin-ops.h"
 #include <stdio.h>
-#include "./../constants/menu.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "admin-ops.h"
+#include "../constants/menu.h"
+#include "../model/student.h"
+#include "../dao/student-dao.h"
+
+void createStudent(int socket_fd);
 
 void handle_admin_operations(int socket_fd) {
+    printf("in handle_admin_operations \n");
     char read_buffer[1000], write_buffer[1000];
     int bytes_rcvd, bytes_sent;
 
@@ -10,30 +19,56 @@ void handle_admin_operations(int socket_fd) {
 
     strcpy(write_buffer, ADMIN_MENU);
 
-    bytes_sent = write(socket_fd, write_buffer, strlen(write_buffer));
-    if (bytes_sent == -1)
+    if (write(socket_fd, write_buffer, strlen(write_buffer)) == -1)
         perror("Error while displaying admin menu to the user");
-    else {
-        bzero(read_buffer, sizeof(read_buffer));
-        bytes_rcvd = read(socket_fd, read_buffer, sizeof(read_buffer));
-        if (bytes_rcvd == -1) {
-            perror("Error while reading from client");
-        } else {
-            int choice = atoi(reaxd_buffer);
-            printf("Choice: %d \n", choice);
-            switch(choice) {
-                case 1 : 
-                    printf("Add a student");
-                    break;
-                case 2 :
-                    printf("View student details");
-                    break;
-                default :
-                    bzero(write_buffer, sizeof(write_buffer));
-                    strcpy(write_buffer, "Invalid option was selected !!!");
-                    write(socket_fd, write_buffer, strlen(write_buffer));
-                    break;
-            }
-        }
+    printf("admin menu written \n");
+    bzero(read_buffer, sizeof(read_buffer));
+    if (read(socket_fd, read_buffer, sizeof(read_buffer)) == -1) {
+        perror("Error while reading from client");
     }
+    int choice = atoi(read_buffer);
+    printf("Choice: %d \n", choice);
+    switch(choice) {
+        case 1 : 
+            printf("Add a student \n");
+            createStudent(socket_fd);
+            // return yes or no to continue
+            printf("after adding a student \n");
+            break;
+        case 2 :
+            printf("View student details \n");
+            break;
+        default :
+            bzero(write_buffer, sizeof(write_buffer));
+            strcpy(write_buffer, "Invalid option was selected !!!");
+            write(socket_fd, write_buffer, strlen(write_buffer));
+            break;
+    }
+}
+
+void createStudent(int socket_fd) {
+    char read_buffer[1000], write_buffer[1000];
+    int bytes_rcvd, bytes_sent;
+
+    bzero(write_buffer, sizeof(write_buffer));
+    bzero(read_buffer, sizeof(read_buffer));
+
+    strcpy(write_buffer, "Enter the name of the student: ");
+    write(socket_fd, write_buffer, strlen(write_buffer));
+    read(socket_fd, read_buffer, sizeof(read_buffer));
+    char name[50];
+    strcpy(name, read_buffer);
+
+    strcpy(write_buffer, "Enter the age of the student: ");
+    write(socket_fd, write_buffer, strlen(write_buffer));
+    read(socket_fd, read_buffer, sizeof(read_buffer));
+    int age = atoi(read_buffer);
+
+    strcpy(write_buffer, "Enter the email id of the student: ");
+    write(socket_fd, write_buffer, strlen(write_buffer));
+    read(socket_fd, read_buffer, sizeof(read_buffer));
+    char email[50];
+    strcpy(email, read_buffer);
+
+    insertStudent(name, age, email);
 }
