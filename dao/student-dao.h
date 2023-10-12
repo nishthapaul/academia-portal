ce9bdc417d2f297be1aa0a06df291dc3a98546ea
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2023-10-13 03:47:39 +0530

    Add(dao) | Define update student methods in header file

--- dao/student-dao.h
+++ dao/student-dao.h
@@ -4,4 +4,8 @@
 int insertStudent(char name[], int age, char email[]);
 struct Student getStudentDetails(char rollno_string[]);
 
+struct Student updateStudentName(int rollno, char name[]);
+struct Student updateStudentAge(int rollno, int age);
+struct Student updateStudentEmail(int rollno, char email[]);s
+
 #endif
